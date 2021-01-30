b9c2f430c79e072f7beb53ee5754d7fb16e720fa
Frank Borkin <borkinfr@gmail.com>
2021-01-30 11:42:41 +0000

    Output for debugging

--- pdf.php
+++ pdf.php
@@ -1,4 +1,5 @@
 <?php
+/*
 header("Content-type:application/pdf");
 header("Content-Disposition:attachment;filename='handwriting.pdf'");
 require 'vendor/autoload.php';
@@ -7,9 +8,12 @@ use Dompdf\Dompdf;
 
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
+*/
 if (strcmp($_REQUEST['style'],"joined"))
 { $f="Mv Jadheedh Trace"; $n="MvJadheedhTrace-M97e";} else {$f="Learning Curve Dashed"; $n="LearningCurveDashed-w4DP";}
 $html="<html><head><style>@font-face { font-family: '{$f}'; src: url('{$n}.woff')  format('woff'), url('{$n}.ttf')  format('truetype');} body, div {font-family: '{$f}';}</style></head><body><div>{$_REQUEST['story']}<div></body></html>";
+echo $html;
+/*
 $dompdf->loadHtml($html);
 
 // (Optional) Setup the paper size and orientation
@@ -22,4 +26,5 @@ $dompdf->render();
 
 // Output the generated PDF to Browser
 $dompdf->stream();
+*/
 ?>
