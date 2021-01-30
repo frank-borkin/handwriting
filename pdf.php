d013e47ebcc2536d2c9a60f61f9c20b206ea9b36
Frank Borkin <borkinfr@gmail.com>
2021-01-30 11:50:59 +0000

    Try again after installing dompdf fonts

--- pdf.php
+++ pdf.php
@@ -1,5 +1,4 @@
 <?php
-/*
 header("Content-type:application/pdf");
 header("Content-Disposition:attachment;filename='handwriting.pdf'");
 require 'vendor/autoload.php';
@@ -8,12 +7,12 @@ use Dompdf\Dompdf;
 
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
-*/
+
 if (strcmp($_REQUEST['style'],"joined"))
 { $f="Mv Jadheedh Trace"; $n="MvJadheedhTrace-M97e";} else {$f="Learning Curve Dashed"; $n="LearningCurveDashed-w4DP";}
-$html="<html><head><style>@font-face { font-family: '{$f}'; src: url('{$n}.woff')  format('woff'), url('{$n}.ttf')  format('truetype');} body, div {font-family: '{$f}';}</style></head><body><div>{$_REQUEST['story']}<div></body></html>";
-echo $html;
-/*
+$html="<html><head><style>@font-face { font-family: '{$f}'; src: url('{$n}.woff')  format('woff'), url('{$n}.ttf')  format('truetype');} body, div {font-family: '{$f}'; font-size: 48pt;}</style></head><body><div>{$_REQUEST['story']}<div></body></html>";
+//echo $html;
+
 $dompdf->loadHtml($html);
 
 // (Optional) Setup the paper size and orientation
@@ -26,5 +25,5 @@ $dompdf->render();
 
 // Output the generated PDF to Browser
 $dompdf->stream();
-*/
+
 ?>
